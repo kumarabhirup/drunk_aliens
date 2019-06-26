12c12f2ed2104e7208ebd3de9933a20667eb11d8
Svarog1389 <Svarog1389@koji-projects.com>
2019-06-26 13:09:27 +0000

    Manual deploy

--- README.md
+++ README.md
@@ -1,10 +1,73 @@
-# Koji P5 Scaffold
+# Basic Game Template
 
-*A great starting point for any p5 project*
+This is an extension of the P5 Koji Scaffold originally made by [jones](https://gokoji.com/profile/jones).
+
+I've compiled some easily modifiable universal features that I use across most of my game templates, which usually save me at least an hour or two of setting up each template, as well as provide code consistency.
+
+Things like the main menu, sound button, scaling, custom font loading, highscore etc. are all included here which makes it a great starting point for any game.
 
 ## Getting Started
-Write your p5 code [here](#~/frontend/pages/HomePage/index.js).
+Main code is in *frontend/app/index.js*
+
+## About The Game
+
+Controls: How is this game played?
+
+## Scaling
+
+This system for scaling is the one I use across all of my templates.
+
+It's mostly based on using the *objSize* variable that is calculated when the app starts.
+
+Basically, instead of using hardcoded pixels to draw objects, you use *objSize*.
+
+To draw an object you would do something like:
+
+*image(myImage, pos.x, pos.y, objSize, objSize);*
+
+Which would make sure it scales properly relative to screen size.
+If you want bigger or smaller objects, just multiply *objSize* with a value.
+
+You can also use *objSize* to determine positions:
+
+*pos.x = width/2 + objSize * 2;*
+
+Would offset the X coordinate by two objSizes right of the center.
+
+Have fun playing with this, it's really simple once you get the hang of it and could really simplify your code!
+
+
+
+# Customizations
+
+Include tips for the best types of assets in each customization section.
+
+Things that can be customized:
+
+## Colors
+
+- Background
+- Title
+- Instructions
+- Play Button
+- Score and High Score
+
+
+## Images
+- Background (optional)
+- Life Icon
+
+## Sounds
+- Music
+
+## Game Settings
+- Any Google Font can be used
+- All game text
+- Lives
+
+## NOTES
+
+Avoid resizing the game window while playing, weird stuff happens.
 
 ## Support and suggestions
-Please feel free to [contact me](https://gokoji.com/profile/jones) if you have any suggestions!   
-
+Please feel free to [contact me](https://gokoji.com/profile/Svarog1389) if you have any suggestions!   
--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,56 +1,339 @@
-let img;
-let button;
-let sounds; // sounds
+let myFont; //The font we'll use throughout the app
 
+let gameOver = false; //If it's true the game will render the main menu
+let gameBeginning = true; //Should be true only before the user starts the game for the first time
+
+//===Game objects
+//Declare game objects here like player, enemies etc
+
+//===Buttons
+let playButton;
+let soundButton;
+
+
+//===Score data
+let score = 0;
+let highScore = 0;
+let highscoreGained = false;
+let scoreGain;
+
+//===Data taken from Game Settings
+let startingLives;
+let lives;
+
+//===Images
+let imgLife;
+let imgBackground;
+
+//===Audio
+let sndMusic;
+
+let soundEnabled = true;
+let canMute = true;
+
+let soundImage;
+let muteImage;
+
+
+//===Size stuff
+let objSize; //base size modifier of all objects, calculated based on screen size
+
+//game size in tiles, using bigger numbers will decrease individual object sizes but allow more objects to fit the screen
+//Keep in mind that if you change this, you might need to change text sizes as well
+let gameSize = 18;
+
+let isMobile = false;
+let touching = false; //Whether the user is currently touching/clicking
+
+//===This function is called before starting the game
 function preload() {
+    //===Load font from google fonts link provided in game settings
+    var link = document.createElement('link');
+    link.href = Koji.config.strings.fontFamily;
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+    myFont = getFontFamily(Koji.config.strings.fontFamily);
+    let newStr = myFont.replace("+", " ");
+    myFont = newStr;
+    //===
 
-  // setup sounds
-  soundFormats('mp3', 'ogg');
-  sounds = {
-    backgroundMusic: loadSound(Koji.config.sounds.backgroundMusic)
-  };
+    //===Load images
+
+    //Load background if there's any
+    if (Koji.config.images.background != "") {
+        imgBackground = loadImage(Koji.config.images.background);
+    }
+
+    imgLife = loadImage(Koji.config.images.lifeIcon);
+
+    soundImage = loadImage(Koji.config.images.soundImage);
+    muteImage = loadImage(Koji.config.images.muteImage);
+
+    //===Load Sounds
+    sndMusic = loadSound(Koji.config.sounds.backgroundMusic);
+
+    //===Load settings from Game Settings
+    startingLives = parseInt(Koji.config.strings.lives);
+    lives = startingLives;
+
 
 }
-
 function setup() {
+    width = window.innerWidth;
+    height = window.innerHeight;
 
-  // make a full screen canvas
-  createCanvas(window.innerWidth, window.innerHeight);
+    //===How much of the screen should the game take, this should usually be left as it is
+    let sizeModifier = 0.75;
+    if (height > width) {
+        sizeModifier = 1;
+    }
 
-  img = loadImage(Koji.config.images.mouse); // Load the image
+    createCanvas(width, height);
 
+    //Magically determine basic object size depending on size of the screen
+    objSize = floor(min(floor(width / gameSize), floor(height / gameSize)) * sizeModifier);
+
+    isMobile = detectMobile();
+
+    //===Get high score data from local storage
+    if (localStorage.getItem("highscore")) {
+        highScore = localStorage.getItem("highscore");
+    }
+
+    textFont(myFont); //set our font
+
+    playButton = new PlayButton();
+    soundButton = new SoundButton();
+
+    gameBeginning = true;
+
+    //Remove comment if you want the music to start
+    //playMusic();
 
 }
 
 function draw() {
-  // set the background color from the configuration options
-  background(Koji.config.colors.backgroundColor);
 
-  // format our text
-  textSize(24);
-  fill(Koji.config.colors.textColor);
-  textAlign(CENTER);
+    //Draw background or a solid color
+    if (imgBackground) {
+        background(imgBackground);
+    } else {
+        background(Koji.config.colors.backgroundColor);
+    }
 
-  // print out our text
-  text(Koji.config.strings.content, window.innerWidth / 2, 100);
+    //===Draw UI
+    if (gameOver || gameBeginning) {
 
-  // play our background music
-  if (!sounds.backgroundMusic.isPlaying()) {
-    sounds.backgroundMusic.play();
-  }
+        //===Draw title
+        let titleText = Koji.config.strings.title;
+        let titleSize = floor(objSize * 2);
+        textSize(titleSize);
 
-  // setup an image to follow our mouse
-  let imageSize = 100;
-  image(img, mouseX - (imageSize / 2), mouseY - (imageSize / 2), imageSize, imageSize);
+        //Resize title until it fits the screen
+        while (textWidth(titleText) > width * 0.9) {
+            titleSize *= 0.9;
+            textSize(titleSize);
+        }
+
+        fill(Koji.config.colors.titleColor);
+        textAlign(CENTER, TOP);
+        text(Koji.config.strings.title, width / 2, objSize * 3);
+
+        //===Draw instructions
+        let instructionsText = [];
+        instructionsText[0] = Koji.config.strings.instructions1;
+        instructionsText[1] = Koji.config.strings.instructions2;
+        instructionsText[2] = Koji.config.strings.instructions3;
+
+        let instructionsSize = [];
+
+        for (let i = 0; i < instructionsText.length; i++) {
+            instructionsSize[i] = floor(objSize * 0.75);
+            textSize(instructionsSize[i]);
+
+            //Resize text until it fits the screen
+            while (textWidth(instructionsText[i]) > width * 0.9) {
+                instructionsSize[i] *= 0.9;
+                textSize(instructionsSize[i]);
+            }
+        }
+
+        textSize(instructionsSize[0]);
+        fill(Koji.config.colors.instructionsColor);
+        textAlign(CENTER, TOP);
+        text(instructionsText[0], width / 2, objSize * 6);
+
+        textSize(instructionsSize[1]);
+        fill(Koji.config.colors.instructionsColor);
+        textAlign(CENTER, TOP);
+        text(instructionsText[1], width / 2, objSize * 8);
+
+        textSize(instructionsSize[2]);
+        fill(Koji.config.colors.instructionsColor);
+        textAlign(CENTER, TOP);
+        text(instructionsText[2], width / 2, objSize * 10);
+
+
+        //===
+        playButton.update();
+        playButton.btn.draw();
+
+        //===Draw score text after the game
+        if (!gameBeginning) {
+            textSize(objSize * 0.9);
+            fill(Koji.config.colors.scoreColor);
+            textAlign(CENTER, TOP);
+            text(Koji.config.strings.scoreText + " " + score, width / 2, playButton.pos.y + objSize * 4);
+        }
+
+        //===Notify the player if they got a new high score, otherwise show the previous high score
+        if (highscoreGained) {
+            textSize(objSize * 1);
+            fill(Koji.config.colors.highscoreColor);
+            textAlign(CENTER, BOTTOM);
+            text(Koji.config.strings.highscoreGainedText, width / 2, height - objSize);
+        } else {
+            textSize(objSize * 1);
+            fill(Koji.config.colors.highscoreColor);
+            textAlign(CENTER, BOTTOM);
+            text(Koji.config.strings.highscoreText + "\n" + highScore, width / 2, height - objSize);
+        }
+    } else {
+
+
+        //Update and render all game objects here
+        
+
+        //===Update all floating text objects
+        for (let i = 0; i < floatingTexts.length; i++) {
+            floatingTexts[i].update();
+            floatingTexts[i].render();
+        }
+
+        //===Ingame UI
+
+        textSize(objSize * 1);
+        fill(Koji.config.colors.scoreColor);
+        textAlign(CENTER, CENTER);
+        text("Game happens here!", width / 2, height / 2);
+
+
+        //===Score draw
+        let scoreX = width - objSize / 2;
+        let scoreY = objSize / 3;
+        textSize(objSize * 2);
+        fill(Koji.config.colors.scoreColor);
+        textAlign(RIGHT, TOP);
+        text(score, scoreX, scoreY);
+
+        //Lives draw
+        let lifeSize = objSize;
+        for (let i = 0; i < lives; i++) {
+            image(imgLife, lifeSize/2 + lifeSize * i, lifeSize/2, lifeSize, lifeSize);
+        }
+
+        cleanup();
+
+    }
+
+    soundButton.render();
 }
 
 
-// start audio context
-function touchStarted() {
-  if (getAudioContext().state !== 'running') {
-    getAudioContext().resume();
-  }
+//===Go through objects and see which ones need to be removed
+//A good practive would be for objects to have a boolean like removable, and here you would go through all objects and remove them if they have removable = true;
+function cleanup() {
+    for (let i = 0; i < floatingTexts.length; i++) {
+        if (floatingTexts[i].timer <= 0) {
+            floatingTexts.splice(i, 1);
+        }
+    }
+}
 
-  var synth = new p5.MonoSynth();
-  synth.play('A4', 0.5, 0, 0.2);
+
+//===Handle input
+function touchStarted() {
+
+    if (gameOver || gameBeginning) {
+
+    }
+
+    if (soundButton.checkClick()) {
+        toggleSound();
+        return;
+    }
+
+    if (!gameOver && !gameBeginning) {
+        //Ingame
+        touching = true;
+
+    }
+}
+
+function touchEnded() {
+    touching = false;
+}
+
+function keyPressed() {
+    if (!gameOver && !gameBeginning) {
+
+    }
+}
+
+function keyReleased() {
+    if (!gameOver && !gameBeginning) {
+
+    }
+}
+
+//===Call this every time you want to start or reset the game
+//This is a good place to clear all arrays like enemies, bullets etc before starting a new game
+function init() {
+    gameOver = false;
+
+    highscoreGained = false;
+    score = 0;
+
+    floatingTexts = [];
+
+}
+
+//===Call this when a lose life event should trigger
+function loseLife() {
+
+    lives--;
+    if (lives <= 0) {
+        gameOver = true;
+
+        checkHighscore();
+    }
+
+}
+
+
+//===The way to use Floating Text:
+//floatingTexts.push(new FloatingText(...));
+//Everything else like drawing, removing it after it's done etc, will be done automatically
+function FloatingText(x, y, txt, color, size) {
+    this.pos = createVector(x, y);
+    this.size = 1;
+    this.maxSize = size;
+    this.timer = 1;
+    this.txt = txt;
+    this.color = color;
+
+    this.update = function () {
+        if (this.size < this.maxSize) {
+            this.size = Smooth(this.size, this.maxSize, 2);
+        }
+
+        this.timer -= 1 / frameRate();
+    }
+
+    this.render = function () {
+        textSize(this.size);
+        fill(this.color);
+        textAlign(CENTER, BOTTOM);
+        text(this.txt, this.pos.x, this.pos.y);
+    }
 }
